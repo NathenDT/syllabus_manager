6f288a465df39ce4b55dff5994291243f8640046
Nathen dela Torre <nathendelatorre@gmail.com>
2024-02-11 01:20:08 -0800

    Some changes

---
+++ client/src/App.tsx
@@ -0,0 +1,21 @@
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import Typography from '@mui/material/Typography'
+
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <Typography variant="h2" margin={1}>
+        Hello
+      </Typography>
+
+      <TextField />
+
+      <Button>a</Button>
+    </>
+  )
+}
+
+export default App
